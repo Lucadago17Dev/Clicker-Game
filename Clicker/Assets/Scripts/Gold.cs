fc447dbc32a149a8c8edd8da35b1ed42bbf3c300
Lucadago17Dev <49600180+Lucadago17Dev@users.noreply.github.com>
2019-04-14 02:28:03 +0200

    Add Project

---
+++ Clicker/Assets/Scripts/Gold.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : MonoBehaviour
+{
+  
+
+    public static float gold = 500.0f;
+    public int cookies;
+    public float mult = 1.0f;
+    public float baseClick = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cookies = addCookies.cookies;
+        baseClick = cookies * .2f;
+
+    }
+
+
+
+
+    private void OnMouseDown()
+    {
+        gold = gold + (baseClick * mult);
+
+    }
+
+
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), gold.ToString("#.00"));
+    }
+}
---
+++ Clicker/Assets/Scripts/addCookies.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addCookies : MonoBehaviour
+{
+    public static int cookies = 0;
+    float gold;
+    public float cost = 5.0f;
+    float costMult = 2.0f;
+
+    private void Update()
+    {
+        gold = Gold.gold;
+    }
+    private void OnMouseDown()
+    {
+        
+        if (gold >= cost)
+        {
+            cookies++;
+            Gold.gold -= cost;
+            cost = cost + (cookies * costMult);
+
+        }
+        Debug.Log("gold = " + gold);
+    }
+
+
+}

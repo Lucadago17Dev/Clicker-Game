d37115a8d5d45a46073c161be838723d137e1715
Lucadago17Dev <49600180+Lucadago17Dev@users.noreply.github.com>
2019-04-14 14:25:10 +0200

    Clicker_ver2

--- Clicker/Assets/Scripts/Gold.cs
+++ Clicker/Assets/Scripts/Gold.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Gold : MonoBehaviour
 {
-  
+
 
     public static float gold = 500.0f;
     public int cookies;
     public float mult = 1.0f;
     public float baseClick = 20;
 
-    // Update is called once per frame
-    void Update()
+    public GUIStyle guiStyle;
+   
+// Update is called once per frame
+void Update()
     {
         cookies = addCookies.cookies;
         baseClick = cookies * .2f;
@@ -28,10 +30,10 @@ public class Gold : MonoBehaviour
 
     }
 
-
-
+   
     void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 100, 20), gold.ToString("#.00"));
+        guiStyle.fontSize = 20;
+        GUI.Label(new Rect(10, 10, 100, 20),"Money : "+ gold.ToString("#.00"),guiStyle);
     }
 }
